c5f4918261b8bb98115f7667c90950240a0b6845
Claus Wahlers <claus@codeazur.com.br>
2013-12-16 18:38:46 -0200

    approximate behavior of generateFilterRect for blur based filters

--- src/flash/filters/BitmapFilter.js
+++ src/flash/filters/BitmapFilter.js
@@ -17,11 +17,45 @@
  */
 
 var BitmapFilterDefinition = (function () {
+  var EPS = 0.000000001;
+  var blurFilterStepWidths = [
+    2,
+    1 / 1.05,
+    1 / 1.35,
+    1 / 1.55,
+    1 / 1.75,
+    1 / 1.9,
+    1 / 2,
+    1 / 2.1,
+    1 / 2.2,
+    1 / 2.3,
+    1 / 2.5,
+    1 / 3,
+    1 / 3,
+    1 / 3.5,
+    1 / 3.5
+  ];
   var def = {
     __class__: 'flash.filters.BitmapFilter',
 
     initialize: function () {
 
+    },
+    _updateBlurBounds: function (bounds, isBlurFilter) {
+      var stepWidth = blurFilterStepWidths[this._quality - 1] - eps;
+      var bx = this._blurX;
+      var by = this._blurY;
+      if (isBlurFilter) {
+        var stepWidth4 = stepWidth / 4;
+        bx -= stepWidth4;
+        by -= stepWidth4;
+      }
+      var bh = Math.ceil((bx < 1 ? 1 : bx) / stepWidth);
+      var bv = Math.ceil((by < 1 ? 1 : by) / stepWidth);
+      bounds.xmin -= bh;
+      bounds.xmax += bh;
+      bounds.ymin -= bv;
+      bounds.ymax += bv;
     },
     _updateFilterBounds: function (bounds) {
 
